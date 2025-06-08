a6a57c1b07e0cf47b674e64fa2cadd89d45a566f
VitorTxe <vitormateus1996@hotmail.com>
2025-06-08 19:04:04 -0300

    Criado pastas para organizar os projetos

---
+++ script/menu-script copy.js
@@ -0,0 +1,11 @@
+let btnMenu = document.querySelector("#btn-abrir-menu");
+let menu = document.querySelector("#menu-mobile");
+let overlay = document.querySelector("#overlay-menu");
+
+btnMenu.addEventListener("click", () => {
+  menu.classList.add("abrir-menu");
+});
+
+menu.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
